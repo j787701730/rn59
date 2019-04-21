516f1e1dc9bae2c9bef21b53d62a176539af1fd6
j787701730 <787701730@qq.com>
2019-04-21 18:20:42 +0800

    gradle

--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,7 @@ import {View, Text, Button} from "react-native";
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    headerTitle: <Text>home</Text>,
+    headerTitle: <Text style={{fontWeight: 'bold'}}>home</Text>,
     headerRight: (
       <Button
         onPress={() => alert('This is a button!')}
@@ -17,7 +17,7 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-        <Text>Home Screen</Text>
+        <Text>Home Screenq</Text>
       </View>
     );
   }
